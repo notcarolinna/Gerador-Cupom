2ee6a8f36a46f8198d5505dee66948de84d6aada
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-08 20:56:03 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Empresa.h"
+#include "EmpresaService.h"
+#include "EmpresaController.h"
+#include "Application.h"
+
+int main() {
+    Application *app = new Application();
+    app->init();
+}
