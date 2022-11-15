08c9c3c2649f7301424d181748a794062909b47b
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-15 16:55:45 -0300

    Update Application.h

--- Application.h
+++ Application.h
@@ -1,17 +1,27 @@
-#ifndef APPLICATION_H
-#define APPLICATION_H
+#ifndef DESCONTO_h
+#define DESCONTO_h
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 #pragma once
 
-class Application
-{
+class Desconto {
+
+	int tipo;
+	string codigo_barras_a; //campo 1
+	string valor_desc; // campo 2
+	string num_itens_levados; //campo 3
+	string num_itens_pagos; //campo 4
+	string codigo_barras_b; //campo 5
+	string desc_prod_b; // campo 6
+
 public:
-    Application();
-    ~Application();
-    void init();
-private:
-    EmpresaController *empresaController;
-    Empresa *empresa;
+	Desconto(string, string);
+	Desconto(string, string, string);
+	Desconto(string, string, string, string, string);
+
 };
 
 #endif
