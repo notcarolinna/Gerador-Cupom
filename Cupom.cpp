1169c73771f1d663801d43ea404d3f375f731700
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-24 01:20:19 -0300

    Update Cupom.cpp

--- Cupom.cpp
+++ Cupom.cpp
@@ -1,38 +1,41 @@
 #include "Cupom.hpp"
+#include <fstream>
 #include <sstream>
 
-void Cupom::getCupom() {
- 
-std::ifstream infile("data/numcupom.csv");
-std::string line; 
+Cupom::Cupom() {
 
-  std::string numero;
-  std::string serie;
+  std::ifstream infile("data/numcupom.csv");
+  std::string line;
 
-while(std::getline(infile, line)){
-   int campo = 0;
+  while (std::getline(infile, line)) {
+    int campo = 0;
     std::string delimiter = ";";
     line = line + delimiter;
     size_t pos = 0;
     std::string wait;
-    while((pos = line.find(delimiter)) != std::string::npos){
-        wait = line.substr(0, pos);
-        line.erase(0, pos + delimiter.length());
-        if(campo == 0){
-            numero = wait;
-        } else if(campo == 1){
-            serie = wait;
+    while ((pos = line.find(delimiter)) != std::string::npos) {
+      wait = line.substr(0, pos);
+      line.erase(0, pos + delimiter.length());
+      if (campo == 0) {
+        numero = wait;
+      } else if (campo == 1) {
+        serie = wait;
       }
       campo++;
     }
   }
-
-std::string Cupom::getNumero(){
-  int num = stoi(numero); //stoi connverte string pra inteiro
-   return this->num;
 }
 
-std::string Cupom::getSerie(){
+int Cupom::getNumero() {
+  num = stoi(numero); 
+  return num;
+}
+
+int Cupom::getSerie() {
   int ser = stoi(serie);
-   return this->ser;
+  return ser;
+}
+
+void Cupom::aumentanum(){
+  num++;
 }
