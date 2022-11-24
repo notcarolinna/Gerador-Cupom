bf38637e85bc898ba2c2fd45d5da630586b0c839
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-24 01:24:23 -0300

    Update Endereco.cpp

--- Endereco.cpp
+++ Endereco.cpp
@@ -1,65 +1,51 @@
-#include <string>
 #include <iostream>
+#include <string>
 
-#include "Endereco.h"
+#include "Endereco.hpp"
 
 using namespace std;
 
-Endereco::Endereco(string line) {
+Endereco::Endereco(std::string line) {
 
-    int aux = 0;
-    string delimiter = ":";
-    line = line + delimiter;
-    size_t pos = 0;  // size_t é tipo um int, só q pra tamanhos de objetos
-    string hold; // a string q armazena temporariamente os valores
-    while ((pos = line.find(delimiter)) != string::npos) { // npos = é tipo o \0 , indica o fim da string
-        hold = line.substr(0, pos);
-        line.erase(0, pos + delimiter.length());
+  int aux = 0;
+  std::string delimiter = ":";
+  line = line + delimiter;
+  size_t pos = 0;   
+  std::string hold; 
+  while ((pos = line.find(delimiter)) !=
+         std::string::npos) { 
+    hold = line.substr(0, pos);
+    line.erase(0, pos + delimiter.length());
 
-        if (aux == 0) {
-            logradouro = hold;
-        }
-        else if (aux == 1) {
-            numero = hold;
-        }
-        else if (aux == 2) {
-            complemento = hold;
-        }
-        else if (aux == 3) {
-            bairro = hold;
-        }
-        else if (aux == 4) {
-            cidade = hold;
-        }
-        else if (aux == 5) {
-            cep = hold;
-        }
-        aux++;
-    }
+    if (aux == 0) {
+      logradouro = hold;
+    } else if (aux == 1) {
+      numero = hold;
+    } else if (aux == 2) {
+      bairro = hold;
+    } else if (aux == 3) {
+      cidade = hold;      
+    } else if (aux == 4) {
+       uf = hold;
+    } else if (aux == 5) {
+        cep = hold;
+    } 
+    aux++;
+  }
 }
 
-Endereco::~Endereco() { }
+Endereco::~Endereco() {}
 
-string Endereco::getLogradouro() {
-    return this->logradouro;
-}
+std::string Endereco::getLogradouro() { return this->logradouro; }
 
-string Endereco::getNumero() {
-    return this->numero;
-}
+std::string Endereco::getNumero() { return this->numero; }
 
-string Endereco::getComplemento() {
-    return this->complemento;
-}
+std::string Endereco::getComplemento() { return this->complemento; }
 
-string Endereco::getBairro() {
-    return this->bairro;
-}
+std::string Endereco::getBairro() { return this->bairro; }
 
-string Endereco::getCidade() {
-    return this->cidade;
-}
+std::string Endereco::getCidade() { return this->cidade; }
 
-string Endereco::getCep() {
-    return this->cep;
-}
+std::string Endereco::getCep() { return this->cep; }
+
+std::string Endereco::getUf() { return this->uf; }
