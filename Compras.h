1f659ae3bbb7f989f8eed43e7a85551450b1f698
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-24 01:19:44 -0300

    Update Compras.h

--- Compras.h
+++ Compras.h
@@ -1,19 +1,36 @@
-#ifndef CUPOM_HPP
-#define CUPOM_HPP
+#ifndef COMPRAS_HPP
+#define COMPRAS_HPP
 #include <string>
+#include <vector>
 
-class Compras{
-Compras(std::string cod, std::string qtd);
-std::string codigo_barras_p;
-std::string quantidade;
-int linha = 0;
+class Compras {
+
+class Compra {
+  std::string codigo_barras_p;
+  std::string quantidade;
+
+  public:
+  Compra( std::string cod, std::string quant ) {
+    codigo_barras_p = cod;
+    quantidade = quant;
+  }
+  std::string getCod() { return codigo_barras_p; }
+  std::string getQuant() { return quantidade; }
+  void setQuant(int q) { quantidade = std::to_string( q ); }
+
+};
+
+std::vector<Compra*> comprinhas;
 
 public:
-std::string getCodigo();
-std::string getQuantidade();
-void getCompras();
-int getLinha();
+Compras();
+std::string getCodigo(int);
+std::string getQuantidade(int);
+void setQuantidade(int, int);
 
-}
+  int getSize(){
+    return comprinhas.size();
+  }
+};
 
 #endif
